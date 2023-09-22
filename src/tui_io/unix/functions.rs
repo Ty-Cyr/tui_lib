10107480c69557def4dd8ff012fbae1cc282b8fd
tycyr <tyjcyr@gmail.com>
2023-09-22 14:17:31 -0400

    Error Number Fix

--- src/tui_io/unix/functions.rs
+++ src/tui_io/unix/functions.rs
@@ -10,13 +10,28 @@ extern "C" {
     pub fn read(fd: i32, buf: *mut c_void, count: usize) -> isize;
     fn strerror(errno: u32) -> *const i8;
     fn __error() -> *mut u32;
+    fn __errno_location() -> *mut u32;
+}
+
+#[cfg(not(target_os = "macos"))]
+fn errno() -> u32 {
+    unsafe {
+        return *__errno_location();
+    }
+}
+
+#[cfg(target_os = "macos")]
+fn errno() -> u32 {
+    unsafe {
+        return *__error();
+    }
 }
 
 pub fn get_errno_error() -> String {
     let string_pointer: *const i8;
     let result: String;
     unsafe {
-        string_pointer = strerror(*__error());
+        string_pointer = strerror(errno());
         result = CStr::from_ptr(string_pointer)
             .to_str()
             .unwrap_or("Unknown Error")
