4373d032ff2c53892ddd341959b6c7ce2a63b192
Ty-J-Cyr <106984831+Ty-J-Cyr@users.noreply.github.com>
2023-09-29 15:04:52 -0400

    Windows Side Mouse Input Dry

--- src/tui_io/windows_tui_io.rs
+++ src/tui_io/windows_tui_io.rs
@@ -4,7 +4,10 @@ mod windows;
 
 use crate::{
     tui_events::TuiEvents,
-    tui_io::{input_interface::InputInterfaceT, output_interface::OutputInterfaceT},
+    tui_io::{
+        input_interface::InputInterfaceT, mouse_input::MouseInput,
+        output_interface::OutputInterfaceT,
+    },
 };
 
 use windows::constants::{
@@ -22,6 +25,7 @@ use windows::{
 };
 
 use self::windows::structs::INPUT_RECORD;
+
 #[derive(Clone, Copy)]
 pub struct TerminalState {
     console_mode: CONSOLE_MODE,
@@ -73,76 +77,6 @@ impl InputInterface {
         return Ok(*event);
     }
 
-    fn get_u16(&self) -> Option<(u16, char)> {
-        let mut num = 0;
-        loop {
-            let next = self.read_raw_immediate()?;
-            match next as u16 {
-                0x30..=0x39 => {
-                    let digit = next as u16 - 0x30;
-                    if num > u16::MAX {
-                        return None;
-                    }
-                    num *= 10;
-                    if digit > u16::MAX - num {
-                        return None;
-                    }
-                    num += digit;
-                }
-                _ => return Some((num, next)),
-            }
-        }
-    }
-
-    fn get_coordinates(&self) -> Option<(u16, u16, char)> {
-        let Some(';') = self.read_raw_immediate() else {
-            return None;
-        };
-        let Some((x, ';')) = self.get_u16() else {
-            return None;
-        };
-        return match self.get_u16() {
-            Some((y, c)) => Some((x, y, c)),
-            None => None,
-        };
-    }
-
-    fn handle_scroll_event(&self) -> TuiEvents {
-        return match self.read_raw_immediate() {
-            Some('4') => match self.get_coordinates() {
-                Some((x, y, 'M')) => TuiEvents::ScrollUp(x, y),
-                None | Some(_) => TuiEvents::Error,
-            },
-            Some('5') => match self.get_coordinates() {
-                Some((x, y, 'M')) => TuiEvents::ScrollDown(x, y),
-                None | Some(_) => TuiEvents::Error,
-            },
-            None | Some(_) => TuiEvents::Error,
-        };
-    }
-
-    fn handle_mouse_move_event(&self) -> TuiEvents {
-        return match self.read_raw_immediate() {
-            Some('2') => match self.get_coordinates() {
-                Some((x, y, 'M')) => TuiEvents::LeftDrag(x, y),
-                Some(_) | None => TuiEvents::Error,
-            },
-            Some('3') => match self.get_coordinates() {
-                Some((x, y, 'M')) => TuiEvents::MiddleDrag(x, y),
-                Some(_) | None => TuiEvents::Error,
-            },
-            Some('4') => match self.get_coordinates() {
-                Some((x, y, 'M')) => TuiEvents::RightDrag(x, y),
-                Some(_) | None => TuiEvents::Error,
-            },
-            Some('5') => match self.get_coordinates() {
-                Some((x, y, 'm')) => TuiEvents::MouseMove(x, y),
-                Some(_) | None => TuiEvents::Error,
-            },
-            None | Some(_) => TuiEvents::Error,
-        };
-    }
-
     fn handle_escape_sequence(&self) -> TuiEvents {
         match self.read_raw_immediate() {
             Some('[') => {}
@@ -162,40 +96,15 @@ impl InputInterface {
             }
         };
 
-        let result = match self.read_raw_immediate() {
-            Some('0') => match self.get_coordinates() {
-                Some((x, y, 'M')) => TuiEvents::LeftClick(x, y),
-                Some((_, _, 'm')) => TuiEvents::Ignore,
-                Some(_) | None => TuiEvents::Error,
-            },
-
-            Some('1') => match self.get_coordinates() {
-                Some((x, y, 'M')) => TuiEvents::MiddleClick(x, y),
-                Some((_, _, 'm')) => TuiEvents::Ignore,
-                Some(_) | None => TuiEvents::Error,
-            },
-
-            Some('2') => match self.get_coordinates() {
-                Some((x, y, 'M')) => TuiEvents::RightClick(x, y),
-                Some((_, _, 'm')) => TuiEvents::Ignore,
-                Some(_) | None => TuiEvents::Error,
-            },
-            Some('3') => self.handle_mouse_move_event(),
-            Some('6') => self.handle_scroll_event(),
-            None | Some(_) => loop {
-                if let None = self.read_raw_immediate() {
-                    return TuiEvents::Error;
-                };
-            },
-        };
+        let result = self.handle_mouse_events();
         match result {
             TuiEvents::Error | TuiEvents::Ignore => loop {
                 if let None = self.read_raw_immediate() {
                     return result;
-                };
+                }
             },
             _ => return result,
-        };
+        }
     }
 
     fn parse_key_event_data(&self, data: KEY_EVENT_RECORD) -> TuiEvents {
@@ -253,6 +162,8 @@ impl InputInterface {
         }
     }
 }
+
+impl MouseInput for InputInterface {}
 
 impl InputInterfaceT for InputInterface {
     fn new() -> Option<InputInterface> {
